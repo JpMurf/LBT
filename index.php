907a79bb7aeeed7ea7f722053d47982fb35342f3
JpMurf <info@luckybones.uk>
2015-07-08 20:56:16 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>LuckyBones Tips has Closed</title>
+
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:700,400" rel="stylesheet">
+		<link href="styles.css" rel="stylesheet">   
+	</head>
+	<body>
+		<div class="side-bar">
+			<img src="img/LBTlogotop.PNG" alt="Codecourse logo" class="animated fadeIn">
+		</div>
+		<div class="container">
+			<h1 class="title animated fadeIn">We're closed for now, <span style="color: #99cc00;">really sorry.</span></h1>
+
+			<div class="content animated fadeIn">
+				<p>Thanks for visiting, right, we have some explaining to do.
+We are in the process of changing ownership/management, . LuckyBones Tips will be back with a fresh new look and a more interactive site. We will still have input from the previous owners and maybe even their own tips section, they are a great bunch of guys and know their stuff.  BLT V2 is already in progress but isn't quite ready yet.
+</p>
+
+				
+				<h3>So why have you taken the site down?</h3>
+				<p>Simply put, it's easier to get everything ready this way. It means we can focus on getting the new site up and running without dealing with the old website.</p>
+
+				<h3>What can we expect from V2 of LBT and what does V2 mean?</h3>
+				<p>Well, I'm glad you asked, V2 simply means version 2 and we have tons of ideas and plan on implementing many over the coming year. Unlike V1 the new site will be much more than a blog listing all the tips and news, here is a little sneaky preview:
+
+				<ul>
+					<li>Shiny new user accounts</li>
+					<li>Forums (for all those extra bits of info you need to know</li>
+					<li>Ability to post your own tips (build your own following)</li>
+					<li>Up the minute prices from top bookies</li>
+					<li>And much much more :)</li>
+				</ul>
+
+
+				</p>
+
+				<h3>Can we contact you while the site is down?</h3>
+				<p>Of course! We are always happy to hear from our visitors, you can find us at <a href="mailto:info@luckybonestips.uk">info@luckybonestips.uk</a></p>
+				<span class="seperator"></span>
+
+				<p>Finally, thanks so much for waiting. We're hoping LBT V2 is something that will grow into a place where you can share and hone your betting skills.</p>
+
+				<p>Dan (New LBT owner)</p>
+
+				<span class="seperator"></span>
+
+				<a href="http://twitter.com/luckybonestips">Twitter</a>
+				</div>
+
+		</div>
+	</body>
+</html>
